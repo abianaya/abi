a4099ef5c0f52f00c1deff89959ec6d80513aaa6
abianaya <selvaabi20@gmail.com>
2016-12-29 14:05:59 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char str[6],i;
+printf("enter the string");
+scanf("%s",str);
+for(i=0;i<5;i++)
+{
+    printf("\n");
+printf("%s",str);
+}
+getch();
+}
