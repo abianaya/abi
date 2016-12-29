189a98bf0ddad895a067fc8f7abb28f60dc64a0a
abianaya <selvaabi20@gmail.com>
2016-12-29 14:21:59 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i=1,n,sum=0;
+printf("enter the number");
+scanf("%d",&n);
+while(i<=n)
+{
+sum+=i;
+++i;}
+printf("the value is %d",sum);
+}
