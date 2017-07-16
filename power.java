7f3c0a79a9e457e9f4aadaa284bd85826e7c5ed5
abianaya <selvaabi20@gmail.com>
2017-07-16 11:34:56 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,16 @@
+package beginner;
+
+import java.util.Scanner;
+
+public class power {
+	public static void main(String[]srgs)
+	{
+		Scanner scan=new Scanner(System.in);
+	int r=scan.nextInt();
+	int s=scan.nextInt();
+	System.out.println(Math.pow(r,s));
+			
+		
+	}
+
+}
