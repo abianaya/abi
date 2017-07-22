3d4baa3f1ad9ac9230ebbda851e8166b06ba3918
abianaya <selvaabi20@gmail.com>
2017-07-22 14:42:23 +0530

    Add files via upload

---
+++ Duplicate.java
@@ -0,0 +1,25 @@
+package beginner;
+
+import java.util.Scanner;
+
+public class Duplicate {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(a[i]==i)
+			{
+			System.out.println("The number whose value equal to the index is : "+i);
+			}
+		}
+		
+		
+	}
+
+}
