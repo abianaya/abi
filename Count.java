d769a4f3496e25decefb6606e03d472c8ea5a266
abianaya <selvaabi20@gmail.com>
2017-08-04 14:49:19 +0530

    Add files via upload

---
+++ Count.java
@@ -0,0 +1,23 @@
+package Example;
+
+import java.util.Scanner;
+
+public class Count {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int t=s.nextInt();
+		int c=3;
+		while(t>c)
+		{
+			t=t-c;
+			c=c*2;
+		}
+		System.out.println(c-t+1);
+		
+	}
+
+
+
+}
+
