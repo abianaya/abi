3d07fb0c5cd5a2c9efa7f5f8d916b1f8086c88a3
abianaya <selvaabi20@gmail.com>
2017-08-04 16:12:28 +0530

    Add files via upload

---
+++ Words.java
@@ -0,0 +1,24 @@
+package SkillRack;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Words {
+	public static void main(String[] args) {
+		int count=0;
+		Scanner s=new Scanner(System.in);
+		
+	
+		String str=s.nextLine();
+		StringTokenizer st=new StringTokenizer(str);
+		System.out.println(str.split("\\s"));
+	while(st.hasMoreTokens())
+		{
+			System.out.println(st.nextToken());
+			count++;
+		}
+		System.out.println(count);
+	}
+}
+
+
